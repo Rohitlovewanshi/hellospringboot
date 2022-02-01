5aeb670ba7fe392f198ffa8944338d169f8ea5d0
Rohitlovewanshi <rohitlovewanshi78@gmail.com>
2022-02-01 14:35:31 +0530

    initial commit

---
+++ src/test/java/com/rohit/springboot/HellospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
